606c1c1027f8cd033cb89a0d0b2211464bd70bab
Iffahnab <iffahnab12@gmail.com>
2016-04-06 14:53:58 +0800

    Changes

--- server.R
+++ server.R
@@ -1,25 +1,26 @@
 library(shiny)
-library(datasets)
+shinyServer(
+  function(input, output){
+    output$rentalYield <- renderText({ calculateRentalYield(input$weeklyRent, input$price) })
+    output$cashflowPerYear <- renderText({calculateYearlyCashflow(input$weeklyRent, input$strataPerQuarter, input$councilPerQuarter, input$waterPerQuarter, input$managementFees, input$weeklyRepayments)})
+    output$cashflowPerWeek <- renderText({calculateWeeklyCashflow(input$weeklyRent, input$strataPerQuarter, input$councilPerQuarter, input$waterPerQuarter, input$managementFees, input$weeklyRepayments)})
+  }
+)
 
-shinyServer(function(input, output) {
-  
-   datasetInput <- reactive({
-    switch(input$dataset,
-           "rock" = rock,
-           "pressure" = pressure,
-           "cars" = cars)
-  })
-  
-   output$caption <- renderText({
-    input$caption
-  })
-  
-  output$summary <- renderPrint({
-    dataset <- datasetInput()
-    summary(dataset)
-  })
-  
-   output$view <- renderTable({
-    head(datasetInput(), n = input$obs)
-  })
-})
+calculateRentalYield <- function (weeklyRent, propertyPrice) 
+{
+  result <- weeklyRent * 52 / propertyPrice * 100
+  return(round(result, digits = 2))
+}
+
+calculateYearlyCashflow <- function(weeklyRent, strata, council, water, managementFees, weeklyRepayments)
+{
+  result <- weeklyRent * 52 - (strata + council + water) * 4 - managementFees * 52 - weeklyRepayments * 52
+  return(round(result, digits = 2))
+}
+
+calculateWeeklyCashflow <- function(weeklyRent, strata, council, water, managementFees, weeklyRepayments)
+{
+  result <- (weeklyRent * 52 - (strata + council + water) * 4 - managementFees * 52 - weeklyRepayments * 52) / 52
+  return(round(result, digits = 2))
+}
